c2b5d16c60f493bc916687a6cbfc378bd18d1b84
Iftikhar Ali Wazir <ifti.wazir36@gmail.com>
2025-10-06 16:03:47 +0530

    update

---
+++ Chapter12/08_main.py
@@ -0,0 +1,7 @@
+from module import myFunc
+
+if __name__ == "__main__":
+    #If this code is directly executed by running the file its present in 
+    print("We are directly running this code")
+    myFunc()
+    print(__name__)
